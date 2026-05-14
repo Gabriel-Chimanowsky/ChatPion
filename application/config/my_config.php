828f8fcd57392693458f6e071eec337b4b273598
Gabriel Chimanowsky <gabriel.trafego.winup@gmail.com>
2026-05-14 09:51:11 -0300

    Adicionando my_config.php que estava faltando

---
+++ application/config/my_config.php
@@ -0,0 +1,58 @@
+<?php 
+$config['default_page_url'] = 'page/blank';
+$config['product_name'] = 'Wegochat_2';
+$config['product_short_name'] = 'Wegochat_2';
+$config['product_version'] = '9.4.3';
+$config['slogan'] = 'Tudo por você';
+
+$config['institute_address1'] = 'Wegochat_2';
+$config['institute_address2'] = 'Localhost';
+$config['institute_email'] = 'admin@admin.com';
+$config['institute_mobile'] = '';
+
+$config['time_zone'] = 'America/Sao_Paulo';
+$config['language'] = 'english';
+
+$config['facebook_poster_image_upload_limit'] = '2';
+$config['facebook_poster_video_upload_limit'] = '15';
+$config['autoreply_image_upload_limit'] = '2';
+$config['autoreply_video_upload_limit'] = '15';
+$config['comboposter_image_upload_limit'] = '2';
+$config['comboposter_video_upload_limit'] = '15';
+$config['vidcaster_image_upload_limit'] = '2';
+$config['vidcaster_video_upload_limit'] = '200';
+$config['messengerbot_image_upload_limit'] = '2';
+$config['messengerbot_video_upload_limit'] = '15';
+$config['messengerbot_audio_upload_limit'] = '5';
+$config['messengerbot_file_upload_limit'] = '15';
+
+$config['email_sending_option'] = 'php_mail';
+$config['enable_tracking_subscribers_last_interaction'] = 'yes';
+$config['is_rtl'] = '0';
+$config['force_https'] = '0';
+$config['enable_signup_form'] = '1';
+$config['instagram_reply_enable_disable'] = '1';
+$config['enable_signup_activation'] = '1';
+$config['enable_support'] = '0';
+
+$config['master_password'] = '';
+
+$config['messengerbot_subscriber_avatar_download_limit_per_cron_job'] = '25';
+$config['messengerbot_subscriber_profile_update_limit_per_cron_job'] = '100';
+
+$config['sms_api_access'] = '';
+$config['email_api_access'] = '';
+$config['enable_open_rate'] = '';
+$config['enable_click_rate'] = '';
+
+$config['persistent_menu_copyright_text'] = 'ChatPion';
+$config['persistent_menu_copyright_url'] = 'http://localhost/chatpeao/';
+
+$config['facebook_poster_group_enable_disable'] = '';
+$config['facebook_poster_botenabled_pages'] = '0';
+$config['central_webhook_verify_token'] = '8784524666';
+$config['sess_use_database'] = FALSE;
+$config['sess_table_name'] = 'ci_sessions';
+$config['mail_service_id'] = '{"mailchimp":[],"sendinblue":[],"activecampaign":[],"mautic":[],"acelle":[],"google_sheet":[]}';
+$config['delete_junk_data_after_how_many_days'] = '30';
+$config['delete_livechat_old_message_day'] = '60';
